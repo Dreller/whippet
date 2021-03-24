f3eeddf82eccc8ca5a0492d37d2b77c4d0d01f0f
dreller <dreller2034@gmail.com>
2021-03-24 19:34:20 -0400

    Dump
    First dump, not working.

---
+++ app.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<?php
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/propeller.min.css">
+    <link rel="stylesheet" href="css/propeller-theme.css">
+    <link rel="stylesheet" href="css/propeller-admin.css">
+</head>
+<body class="">
+   
+<nav class="navbar navbar-inverse navbar-fixed-top pmd-navbar pmd-z-depth">
+	<div class="container-fluid">
+		<div class="pmd-navbar-right-icon pull-right navigation">
+			<!-- Notifications -->
+            <div class="dropdown notification icons pmd-dropdown">
+			
+				<a href="javascript:void(0)" title="Notification" class="dropdown-toggle pmd-ripple-effect" data-toggle="dropdown" role="button" aria-expanded="true">
+					<div data-badge="3" class="material-icons md-light pmd-sm pmd-badge  pmd-badge-overlap">notifications_none</div>
+				</a>
+			
+				<div class="pmd-dropdown-menu-container"><div class="pmd-dropdown-menu-bg"></div><div class="dropdown-menu dropdown-menu-right pmd-card pmd-card-default pmd-z-depth pm-ini" role="menu" style="clip: rect(0px, 320px, 0px, 320px);">
+					<!-- Card header -->
+					<div class="pmd-card-title">
+						<div class="media-body media-middle">
+							<a href="notifications.html" class="pull-right">3 new notifications</a>
+							<h3 class="pmd-card-title-text">Notifications</h3>
+						</div>
+					</div>
+					
+					<!-- Notifications list -->
+					<ul class="list-group pmd-list-avatar pmd-card-list">
+						<li class="list-group-item" style="display:none">
+							<p class="notification-blank">
+								<span class="dic dic-notifications-none"></span> 
+								<span>You don´t have any notifications</span>
+							</p>
+						</li>
+						<li class="list-group-item unread">
+							<a href="javascript:void(0)">
+								<div class="media-left">
+									<span class="avatar-list-img40x40">
+										<img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="themes/images/profile-1.png" data-holder-rendered="true">
+									</span>
+								</div>
+								<div class="media-body">
+									<span class="list-group-item-heading"><span>Prathit</span> posted a new challanegs</span>
+									<span class="list-group-item-text">5 Minutes ago</span>
+								</div>
+							</a>
+						</li>
+						<li class="list-group-item">
+							<a href="javascript:void(0)">
+								<div class="media-left">
+									<span class="avatar-list-img40x40">
+										<img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="themes/images/profile-2.png" data-holder-rendered="true">
+									</span>
+								</div>
+								<div class="media-body">
+									<span class="list-group-item-heading"><span>Keel</span> Cloned 2 challenges.</span>
+									<span class="list-group-item-text">15 Minutes ago</span>
+								</div>
+							</a>
+						</li>
+						<li class="list-group-item unread">
+							<a href="javascript:void(0)">
+								<div class="media-left">
+									<span class="avatar-list-img40x40">
+										<img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="themes/images/profile-3.png" data-holder-rendered="true">
+									</span>
+								</div>
+							
+								<div class="media-body">
+									<span class="list-group-item-heading"><span>John</span> posted new collection.</span>
+									<span class="list-group-item-text">25 Minutes ago</span>
+								</div>
+							</a>
+						</li>
+						<li class="list-group-item unread">
+							<a href="javascript:void(0)">
+								<div class="media-left">
+									<span class="avatar-list-img40x40">
+										<img alt="40x40" data-src="holder.js/40x40" class="img-responsive" src="themes/images/profile-4.png" data-holder-rendered="true">
+									</span>
+								</div>
+								<div class="media-body">
+									<span class="list-group-item-heading"><span>Valerii</span> Shared 5 collection.</span>
+									<span class="list-group-item-text">30 Minutes ago</span>
+								</div>
+							</a>
+						</li>
+					</ul><!-- End notifications list -->
+
+				</div></div>
+				
+				
+            </div> <!-- End notifications -->
+		</div>
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<a href="javascript:void(0);" data-target="basicSidebar" data-placement="left" data-position="slidepush" is-open="true" is-open-width="1200" class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect pull-left margin-r8 pmd-sidebar-toggle"><i class="material-icons md-light">menu</i></a>	
+		  <a href="index.html" class="navbar-brand">
+		  	
+		  </a>
+		</div>
+	</div>
+
+</nav>
+
+
+
+<script src="js/jquery-1.12.2.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/propeller.min.js"></script>
+</body>
+</html>
